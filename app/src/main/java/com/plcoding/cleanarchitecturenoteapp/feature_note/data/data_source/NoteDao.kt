727a7a6bd5c44f5b3bff129ed2905cb194796129
Ronit <97222254+ronit-khalate@users.noreply.github.com>
2023-07-27 19:30:31 +0530

    Note Dao Created

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/data/data_source/NoteDao.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/data/data_source/NoteDao.kt
@@ -1,6 +1,9 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.data.data_source
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
 import kotlinx.coroutines.flow.Flow
@@ -13,5 +16,11 @@ interface NoteDao {
 
     @Query("SELECT * FROM note WHERE id = :id")
     suspend fun getNoteById(id:Int):Note?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
 
 }
