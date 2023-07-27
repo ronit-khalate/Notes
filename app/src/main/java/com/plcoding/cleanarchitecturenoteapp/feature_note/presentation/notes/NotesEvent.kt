96b57c958eecb4566769493034f6623d8704eba1
Ronit <97222254+ronit-khalate@users.noreply.github.com>
2023-07-27 23:19:05 +0530

    property of Order class is changed to NotesState to correct propertie NoteOrder

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesEvent.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesEvent.kt
@@ -1,10 +1,11 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes
 
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.util.NoteOrder
 
 sealed class NotesEvent{
 
-    data class Order(val notesState: NotesState):NotesEvent()
+    data class Order(val notesOrder: NoteOrder):NotesEvent()
     data class DeleteNote(val note: Note):NotesEvent()
     object RestoreNote:NotesEvent()
     object ToggleOrderSection:NotesEvent()
