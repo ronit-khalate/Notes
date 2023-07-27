48a4413291aac1cf7b9e88798dda27f6965ab30d
Ronit <97222254+ronit-khalate@users.noreply.github.com>
2023-07-27 22:18:32 +0530

    mirror spleeing mistake corrected

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesEvent.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesEvent.kt
@@ -5,7 +5,7 @@ import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
 sealed class NotesEvent{
 
     data class Order(val notesState: NotesState):NotesEvent()
-    data class Deletenote(val note: Note):NotesEvent()
+    data class DeleteNote(val note: Note):NotesEvent()
     object RestoreNote:NotesEvent()
     object ToggleOrderSection:NotesEvent()
 
