6a78dd9639cc5b1494dfe0455df6236f6944e815
Ronit <97222254+ronit-khalate@users.noreply.github.com>
2023-07-27 21:23:18 +0530

    only Notesviewmodel class signature created

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesViewModel.kt
@@ -1,4 +1,12 @@
-package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes.compnents
+package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes
 
-class NotesViewModel {
+import androidx.lifecycle.ViewModel
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.NoteUseCasesWrapper
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class NotesViewModel@Inject constructor(
+    private val noteUseCasesWrapper: NoteUseCasesWrapper
+):ViewModel(){
 }
