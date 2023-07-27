be423f4cc6d8f78d7fb234bc4aa9883c5a5cbf1d
Ronit <97222254+ronit-khalate@users.noreply.github.com>
2023-07-27 21:30:55 +0530

    Created NotesEvent scealed class
    This class represent All the intraction of User with UI for example
    order of list , toggle section

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesEvent.kt
@@ -0,0 +1,12 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes
+
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+
+sealed class NotesEvent{
+
+    data class Order(val notesState: NotesState):NotesEvent()
+    data class Deletenote(val note: Note):NotesEvent()
+    object RestoreNote:NotesEvent()
+    object ToggleOrderSection:NotesEvent()
+
+}
